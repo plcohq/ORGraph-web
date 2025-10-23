f7013c8d9ba85739d965ae7f1565b072002f2d2a
zagorodn <zagordn@cs.petrsu.ru>
2025-10-23 17:54:59 +0300

    Удалил вспомогательный скрипт

--- init_structure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . -type d -empty -exec touch {}/.gitkeep \;
-
-echo "# Project Structure" > README.md
-echo "build/\n*.o\n*.so\n*.a" > .gitignore
-
-echo "✅ Project structure created!"
