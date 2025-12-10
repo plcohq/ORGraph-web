b4d168218814d1072765bc7c40682b4ef4a1ea7b
Иван Олюшкин <iv.olyushkin@gmail.com>
2025-12-10 23:53:10 +0300

    Добавил форда беллмана

---
+++ ORGraph/include/algorithms/ford_bellman_algorithm.h
@@ -0,0 +1,9 @@
+#ifndef FORD_BELLMAN_ALGORITHM_H
+#define FORD_BELLMAN_ALGORITHM_H
+
+#include <string>
+#include "Graph.h"
+
+std::string FordBellman(const Graph& graph, int start_vertex);
+
+#endif // FORD_BELLMAN_ALGORITHM_H
